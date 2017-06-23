ebe4c586659d887cc44f621b043992592d2c9dd9
Josmel <jnunez@gmd.com.pe>
2017-06-22 21:34:19 -0500

    SEGUNDA CLASE

---
+++ Prueba.DAO/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Prueba.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.DAO
+{
+    class Contexto: DbContext
+    {
+        public  Contexto():base("Contexto")
+        {
+
+        }
+
+        public DbSet<Clase> Clases{ get; set; }
+
+        public DbSet<Profesor>  Profesores { get; set; }
+    }
+}
---
+++ Prueba.Web/Controllers/ClaseController.cs
@@ -0,0 +1,25 @@
+﻿using Prueba.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prueba.Web.Controllers
+{
+    public class ClaseController : Controller
+    {
+        // GET: Clase
+        public ActionResult Index()
+        {
+            List<Clase> lista = new List<Clase>();
+            Clase cl1 = new Clase() {ClaseID=1, Nombre="hhh",Comentario="aaa"}
+            Clase cl2 = new Clase() { ClaseID = 2, Nombre = "sdf", Comentario = "tyj" }
+            Clase cl3 = new Clase() { ClaseID = 3, Nombre = "qwer", Comentario = "atyjaa" }
+            lista.Add(cl1);
+            lista.Add(cl2);
+            lista.Add(cl3);
+            return View();
+        }
+    }
+}
